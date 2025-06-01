227f98147997fe1e56ee94d6df0e8b993f3d8c1e
JustWiseLove <justwiselove@icloud.com>
2025-05-31 22:52:49 -0400

    Create people.js

---
+++ people.js
@@ -0,0 +1,19 @@
+// people.js
+const people = [
+    {
+        T: "Moses",
+        S: ["Exodus 3:1-10"],
+        D: "Led Israel out of Egypt."
+    },
+    {
+        T: "Abraham",
+        S: ["Genesis 12:1-3"],
+        D: "Father of many nations."
+    },
+    {
+        T: "David",
+        S: ["1 Samuel 17:45-47"],
+        D: "King and psalmist."
+    }
+    // Add more people as needed, in any order
+];
