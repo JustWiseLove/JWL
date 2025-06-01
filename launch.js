1d1d6f25f8e60173fc46c801e60fc1a941834ae7
JustWiseLove <justwiselove@icloud.com>
2025-06-01 17:47:31 -0400

    Update launch.js

--- launch.js
+++ launch.js
@@ -8,5 +8,15 @@ const launch = [
         T: "Pure Worship",
         S: ["James 1:27 - The form of worship that is clean and undefiled from the standpoint of our God and Father is this: to look after orphans and widows in their tribulation, and to keep oneself without spot from the world."],
         D: "Exploring what constitutes pure worship in Jehovah’s eyes and how to apply these principles in daily life."
-    }
+    },
+    {
+        T: "Accuracy",
+        S: ["Psalm 119:160 - The sum of your word is truth, and every one of your righteous judgments endures forever."],
+        D: [
+            {
+                T: "Jehovah, the Only True God",
+                S: ["Psalm 83:18 - That men may know that you, whose name is Jehovah, you alone are the Most High over all the earth.”],
+                D: "Jehovah is the only true God, identified by His name, as Psalm 83:18 declares. Jesus, His Son, is subordinate, sent to reveal Jehovah’s will (John 3:16). Unlike false gods, Jehovah’s sovereignty is proven by His creation and prophecies. Jehovah’s Witnesses honor His name in 240+ lands, distinguishing true worship from idolatry."
+            }
+            ]
 ];
