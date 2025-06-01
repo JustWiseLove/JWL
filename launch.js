2cff389a475ad1faa7a362fa446f206c61746f60
JustWiseLove <justwiselove@icloud.com>
2025-06-01 11:03:35 -0400

    Create launch.js

---
+++ launch.js
@@ -0,0 +1,12 @@
+const launchItems = [
+    {
+        T: "Welcome to JWL",
+        S: ["Psalm 83:18 - That people may know that you, whose name is Jehovah, you alone are the Most High over all the earth."],
+        D: "An introduction to the mission of JWL to guide users in understanding Jehovah’s purpose and worship."
+    },
+    {
+        T: "Pure Worship",
+        S: ["James 1:27 - The form of worship that is clean and undefiled from the standpoint of our God and Father is this: to look after orphans and widows in their tribulation, and to keep oneself without spot from the world."],
+        D: "Exploring what constitutes pure worship in Jehovah’s eyes and how to apply these principles in daily life."
+    }
+];
