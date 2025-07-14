417a13c382f4d7c647894874ba3636ee86ce0eca
JustWiseLove <justwiselove@icloud.com>
2025-07-14 15:53:54 -0400

    Update sidebar.js

--- wire/sidebar.js
+++ wire/sidebar.js
@@ -5,12 +5,12 @@ document.write(`
     </div>
     <nav class="nav">
         <ul class="nav-menu">
-            <li><a href="truth.html" aria-label="Truth page">TRUTH</a></li>
-            <li><a href="people.html" aria-label="People page">PEOPLE</a></li>
-            <li><a href="goodnews.html" aria-label="Good News page">GOOD NEWS</a></li>
+            <li><a href="truth.html" aria-label="Truth page"><i class="fas fa-question-circle"></i> TRUTH</a></li>
+            <li><a href="people.html" aria-label="People page"><i class="fas fa-user"></i> PEOPLE</a></li>
+            <li><a href="goodnews.html" aria-label="Good News page"><i class="fas fa-heart"></i> GOOD NEWS</a></li>
             <li><a href="articles.html" aria-label="Articles page"><i class="fas fa-lightbulb"></i> ARTICLES</a></li>
-            <li><a href="sections.html" aria-label="Sections page">SECTIONS</a></li>
-            <li><a href="topics.html" aria-label="Topics page">TOPICS</a></li>
+            <li><a href="sections.html" aria-label="Sections page"><i class="fas fa-book"></i> SECTIONS</a></li>
+            <li><a href="topics.html" aria-label="Topics page"><i class="fas fa-bookmark"></i> TOPICS</a></li>
         </ul>
     </nav>
 </div>
